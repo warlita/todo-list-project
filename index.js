5df8872f36f2942355255f7eab143b8701045534
Analita <88949609+warlita@users.noreply.github.com>
2024-03-04 22:00:20 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var httpRequest = new XMLHttpRequest();
+httpRequest.onload = function() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      console.log(httpRequest.responseText);
+    } else {
+      console.log(httpRequest.statusText);
+    }
+  }
+}
+httpRequest.onerror = function() {
+  console.log(httpRequest.statusText);
+}
+httpRequest.open('GET', 'https://fewd-todolist-api.onrender.com/tasks?api_key=1212');
+httpRequest.send();
