3d0781db128c511ded515054a7571788ae222b3d
Analita <88949609+warlita@users.noreply.github.com>
2024-03-05 07:43:37 -0600

    learing API

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var httpRequest = new XMLHttpRequest();
-httpRequest.onload = function() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      console.log(httpRequest.responseText);
-    } else {
-      console.log(httpRequest.statusText);
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function (task) {
+        console.log(task.content);
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
     }
-  }
-}
-httpRequest.onerror = function() {
-  console.log(httpRequest.statusText);
-}
-httpRequest.open('GET', 'https://fewd-todolist-api.onrender.com/tasks?api_key=1212');
-httpRequest.send();
+  });
+
+});
