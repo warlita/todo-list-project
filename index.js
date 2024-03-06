84149c7d97b28ad12403d627d61bab2e3fbbd943
Analita <88949609+warlita@users.noreply.github.com>
2024-03-05 22:36:59 -0600

    added in Complete and Active parts of the list

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
         });
       },
       error: function (request, textStatus, errorMessage) {
@@ -55,5 +55,44 @@ $(document).ready(function () {
   $(document).on('click', '.delete', function () {
     deleteTask($(this).data('id'))
   });
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+   type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1212',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    }
+  });
+  var markTaskActive = function (id) {
+    $.ajax({
+   type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1212',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+       markTaskComplete($(this).data('id'));
+     } else {
+       markTaskActive($(this).data('id'));
+     }
+   });
   getAndDisplayAllTasks();
 });
